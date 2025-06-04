eedf39a8fcd786726a5f2576b0dea4cf790ba5e0
korra <korraemail@gmail.com>
2025-06-04 14:46:15 -0500

    made UI

--- interactiveUI/ContentView.swift
+++ interactiveUI/ContentView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      
+        VStack {
+            Text("What is your name?")
+                .font(.title)
+            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.pink, width: 1)
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+
+
+        }
+       // end Vstack
+            .padding()
     }
 }
 
