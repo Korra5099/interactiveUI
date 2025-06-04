9ac445c53e87ede50c2b5553dea7ab1adc7e394c
korra <korraemail@gmail.com>
2025-06-04 14:17:48 -0500

    Initial Commit

---
+++ interactiveUI/interactiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIApp.swift
+//  interactiveUI
+//
+//  Created by Scholar on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
