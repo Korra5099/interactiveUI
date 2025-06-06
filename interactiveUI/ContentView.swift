8fb2287f6da9bc93bf91b9a1d02305995643da5f
korra <korraemail@gmail.com>
2025-06-06 09:27:21 -0500

    good

--- interactiveUI/ContentView.swift
+++ interactiveUI/ContentView.swift
@@ -8,18 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var name  = ""
+    
+    
     var body: some View {
         VStack {
             Text("What is your name?")
                 .font(.title)
-            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Type your name here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
-                .border(Color.pink, width: 1)
+                .border(Color.blue, width: 1)
             Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
             }
-
+//end button
+            .font(.title)
+            .buttonStyle(.borderedProminent)
 
         }
        // end Vstack
